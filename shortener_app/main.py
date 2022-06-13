c0390175ec9498ba43a2b23d1488448e8a5e2bc4
Jason Washburn <jason.washburn@gmail.com>
2022-06-12 20:00:30 -0500

    refactored keygen and crud operations

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,9 +1,9 @@
-import secrets
-
 import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+
+from shortener_app import crud
 
 from . import models, schemas
 from .database import SessionLocal, engine
@@ -36,18 +36,12 @@ def read_root():
 
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
-    if not validators.url(url.target_url):
+    if not validators.url(url.target_url):  # type: ignore
         raise_bad_request(message="Your provided URL is not valid")
 
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
 
     return db_url
 
@@ -56,12 +50,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
 def forward_to_target_url(
     url_key: str, request: Request, db: Session = Depends(get_db)
 ):
-    db_url = (
-        db.query(models.URL)
-        .filter(models.URL.key == url_key, models.URL.is_active)
-        .first()
-    )
-    if db_url:
-        return RedirectResponse(db_url.target_url)
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        return RedirectResponse(db_url.target_url)  # type: ignore
     else:
         raise_not_found(request)
